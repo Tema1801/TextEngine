a3e7fc4517a3d1e45c69447798499491928b0b2c
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 18:50:02 +0300

    Update api.cpp

--- src/core/api.cpp
+++ src/core/api.cpp
@@ -10,7 +10,7 @@ void API::addObject(std::string objectName)
   engine.dataStorage->add_object(objectName);
 }
 
-void API::deleteObject()
+void API::deleteObject(std::string objectName)
 {
   engine.dataStorage->delete_object(objectName);
 }
