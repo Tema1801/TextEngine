7c900648ea3732c44fb9531f3eabb8691a0eb5a7
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 18:59:26 +0300

    Update Object.cpp

--- src/core/storage/Object.cpp
+++ src/core/storage/Object.cpp
@@ -1,4 +1,5 @@
 #include "Object.h"
+#include <iostream>
 
 Object::Object()
 {
@@ -14,3 +15,12 @@ Object::~Object()
 {
 
 }
+
+void showData(std::string objName)
+{
+  std::cout << "Name " << object.name <<std::endl;
+  std::cout << "Health " << object.health <<std::endl;
+  std::cout << "Position "<< object.position << std::endl;
+  std::cout << "Attack power "<< object.atk_power << std::endl;
+  std::cout << "Attack range "<< object.atk_range << std::endl;
+};
