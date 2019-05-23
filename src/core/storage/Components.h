3775986eef1439796e53b0728d49f9bae3b98fb7
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 17:01:22 +0300

    Create Components.h

---
+++ src/core/storage/Components.h
@@ -0,0 +1,13 @@
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+
+class Components
+{
+  public:
+    class Object;
+    Object* objectName;
+    Components();
+    std::string componentType;
+};
+
+#endif //COMPONENTS_H
