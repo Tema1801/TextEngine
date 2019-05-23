87aa10c93224d0f506f1ec940b5428ee198915a0
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 18:08:43 +0300

    Update Info.h

--- src/core/storage/Info.h
+++ src/core/storage/Info.h
@@ -1,1 +1,21 @@
+#ifndef INFO_H
+#define INFO_H
 
+class Components;
+
+clss Info
+{
+  public:
+    std::string name;
+    std::string role;
+    std::Vector<std::string> actionsDescription;
+    std::Vector<std::string> actions;
+    int position;
+    int health;
+    int atk_power;
+    int atk_range;
+    int effect;
+};
+
+
+#endif //INFO_H
