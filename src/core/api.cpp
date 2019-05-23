d8f7393df290386ac75a6b99131cfcf87554fb88
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 18:31:19 +0300

    Update api.cpp

--- src/core/api.cpp
+++ src/core/api.cpp
@@ -21,10 +21,6 @@ void API::addComponent(std::string objectName, std::string componentType)
   {
     engine.datastorage->object[objectName].components.push_back(new Script)
   }
-  if (componentType == "Info")
-  {
-    engine.datastorage->object[objectName].components.push_back(new Info)
-  }
 }
 
 void API::changeComponent(std::string objectName, std::string componentType, std::string componentName)
@@ -33,10 +29,5 @@ void API::changeComponent(std::string objectName, std::string componentType, std
   {
     engine.datastorage->object[objectName].components.erase(std::remove(vec.begin(), vec.end(), componentName), vec.end())
     engine.datastorage->object[objectName].components.push_back(new Script)
-  }
-  if (componentType == "Info")
-  {
-    engine.datastorage->object[objectName].components.erase(std::remove(vec.begin(), vec.end(), componentName), vec.end())
-    engine.datastorage->object[objectName].components.push_back(new Info)
   }
 }
