6c19726bef59b3111edf0972a3f5dc68645837b5
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 17:47:09 +0300

    Update Script.h

--- src/core/storage/Script.h
+++ src/core/storage/Script.h
@@ -1,1 +1,17 @@
+#ifndef SCRIPT_H
+#define SCRIPT_H
+//#include
 
+class Components;
+
+class Script:
+{
+  public:
+    Script();
+    ~Script();
+    void contact();
+    void command(sf::Event event);
+  
+};
+
+#endif //SCRIPT_H
