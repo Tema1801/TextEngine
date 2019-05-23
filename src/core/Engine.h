cb2a8421846e3701a0dae00c82066941a054ee94
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 17:56:36 +0300

    Create Engine.h

---
+++ src/core/Engine.h
@@ -0,0 +1,17 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+//#include
+
+//class ;
+
+class Engine:
+{
+  Engine();
+  ~Engine();
+  DataStorage* dataStorage;
+  LogicManager* logicManager;
+  
+}
+
+#endif //ENGINE_H
