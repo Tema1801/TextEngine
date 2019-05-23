a75fd651c1b84028f0c4b36ee3f641273bef04df
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 16:22:48 +0300

    Create Engine.h

---
+++ src/core/Engine.h
@@ -0,0 +1,11 @@
+#include <string>
+//include h files
+
+class Engine
+{
+  void addObject();
+  void deleteObject();
+  void addComponentToObject(std::string objectName, std::string componentName);
+};
+
+#ENDIF //ENGINE_H
