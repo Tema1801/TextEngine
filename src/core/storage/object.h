73842be489cd7287eb50ae26cf487bd12749cd47
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-23 16:48:49 +0300

    Create object.h

---
+++ src/core/storage/object.h
@@ -0,0 +1,19 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class Object
+{
+  public:
+    Object();
+    std::vector<std::string> components;
+    
+  
+  
+
+
+
+#endif //OBJECT_H
