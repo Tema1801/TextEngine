afae0d5cf75214cf4d7967cc758718d2dad8fb7f
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 21:15:30 +0300

    Update and rename logicmanager.h to LogicManager.h

---
+++ src/core/management/LogicManager.h
@@ -0,0 +1,15 @@
+#ifndef LOGICMANAGER_H
+#define LOGICMANAGER_H
+#include <string>
+//include h files
+
+class LogicManager
+{
+  LogicManager();
+  sf::Event event;
+  void chooseScripts();
+  void executeScripts();
+}
+
+
+#endif //LOGICMANAGER_H
