3dc7bfeecd67a3d86ff8b4bbe431ede35f73b985
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 17:55:09 +0300

    Create dataStorage.h

---
+++ src/core/storage/dataStorage.h
@@ -0,0 +1,19 @@
+#ifndef DATASTORAGE_H
+#define DATASTORAGE_H
+
+#include "engine.h"
+#include "storage/Object.h"
+#include <vector>
+
+class DataStorage
+{
+  public:
+    DataDtorage();
+    ~DataDtorage();
+    std::Vector<Object> objects;
+    void add_object(std::string objectName);
+    void delete_object(std::string objectName);
+    void find_object(std::string objectName);
+};
+
+#endif //DATASTORAGE_H
