76f020ea6d5be5551d488205ad97e927ee4528a8
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 18:37:57 +0300

    Update api.h

--- src/core/api.h
+++ src/core/api.h
@@ -10,6 +10,8 @@ class API
   void addObject(std::string objectName);
   void deleteObject(std::string objectName);
   int findObject(std::string objectName);
+  int getStat(std::string objectName, std::string statName);
+  void setstat(std::string objectName, std::string statName, int value);
 };
 
 #ENDIF //API_H
