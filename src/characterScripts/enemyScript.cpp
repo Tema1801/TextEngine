d33a155fcb482e950f8d438b9bb2efd7c2f21980
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-25 00:17:50 +0300

    Create enemyScript.cpp

---
+++ src/characterScripts/enemyScript.cpp
@@ -0,0 +1,25 @@
+#include "Script.h"
+#include "../core/api.h"
+
+
+Script::Script()
+{
+  
+}
+
+Script::~Script()
+{
+  
+}
+
+void startScripts()
+{
+  health = 10;
+  position = 5;
+  power = 1;
+  range = 1;
+}
+void repeatedScripts()
+{
+  //
+}
