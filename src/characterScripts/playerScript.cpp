b648d6210e74d6ab91c28844f60ad6aaaa00538d
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 23:41:16 +0300

    Update and rename Script.cpp to playerScript.cpp

---
+++ src/characterScripts/playerScript.cpp
@@ -0,0 +1,25 @@
+#include "Script.h"
+#include "../core/api.h"
+
+
+Script::Script()
+{
+  
+}
+
+Script::~Script()
+{
+  
+}
+
+void startScripts()
+{
+  health = 10;
+  position = 0;
+  power = 2;
+  range = 2;
+}
+void repeatedScripts()
+{
+  //
+}
