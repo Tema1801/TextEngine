05287c3856d94339db230257258cd1b3189aa435
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 18:28:44 +0300

    Create Script.cpp

---
+++ src/core/storage/Script.cpp
@@ -0,0 +1,17 @@
+#include "Script.h"
+
+Script::Script()
+{
+  
+}
+
+Script::~Script()
+{
+  
+}
+
+void attack(std::string atkName, std::string tgtName)
+{
+  int attacker = 
+  int distance = abs(
+}
