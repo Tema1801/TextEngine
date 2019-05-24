aab38d8e1b4ef640396ff48dd88e4d742a738cba
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 19:24:22 +0300

    Update Script.cpp

--- src/characterScripts/Script.cpp
+++ src/characterScripts/Script.cpp
@@ -1,4 +1,7 @@
 #include "Script.h"
+#include "../core/api.h"
+
+API API;
 
 Script::Script()
 {
@@ -12,6 +15,19 @@ Script::~Script()
 
 void attack(std::string atkName, std::string tgtName)
 {
-  int attacker = 
-  int distance = abs(
+  int distance = abs(API.getPosition(atkName) - API.getPosition(tgtName));
+  if (distance <= API.getRange(atkName))
+  {
+    API.setHealth(tgtName, API.getHealth(tgtName) - API.getPower(atkName));
+  }
+}
+
+void move(std::string Name, int value)
+{
+  API.setPosition(std::string objectName, value)
+}
+
+void command(std::string Name, sf::Event event)
+{
+  
 }
