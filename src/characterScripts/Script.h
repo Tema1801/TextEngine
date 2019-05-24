488bf413e1224fc6ee1ac0120f4cedc541661229
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-05-24 18:29:05 +0300

    Create Script.h

---
+++ src/characterScripts/Script.h
@@ -0,0 +1,16 @@
+#ifndef SCRIPT_H
+#define SCRIPT_H
+//#include
+
+
+class Script: public Object
+{
+  public:
+    Script();
+    ~Script();
+    void attack(std::string atkName, std::string tgtName);
+    void command(sf::Event event);
+  
+};
+
+#endif //SCRIPT_H
